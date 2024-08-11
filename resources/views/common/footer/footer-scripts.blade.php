f263a8e325bf40467cc76702108eb89d4e7a06d9
Deep <ghoreladeep@gmail.com>
2024-08-11 23:49:36 +0530

    Pagespeed issue fixed

--- resources/views/common/footer/footer-scripts.blade.php
+++ resources/views/common/footer/footer-scripts.blade.php
@@ -1,9 +1,9 @@
 <script src="{!! getAssetPath('assets/js/jquery.min.js') !!}"></script>
 @if($slug == 'contact-us')
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyANn98-tSjlT04-HPHlVNcAIvA_TPEg9KU"></script>
-    <script src="{!! getAssetPath('assets/js/map.js') !!}"></script>
+    <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyANn98-tSjlT04-HPHlVNcAIvA_TPEg9KU"></script>
+    <script async src="{!! getAssetPath('assets/js/map.js') !!}"></script>
 @elseif($slug == 'blogs')
-    <script src="{!! getAssetPath('assets/js/blogs.js') !!}"></script>
+    <script async src="{!! getAssetPath('assets/js/blogs.js') !!}"></script>
 @endif
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-1WJSLMZCZW"></script>
 <script async src="{!! getAssetPath('assets/js/common.js') !!}"></script>
